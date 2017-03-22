e2ca462d4511d12bfa2b6b6e0d2f8bc2bee8931b
Moritz Hoffmann <moritz.hoffmann@inf.ethz.ch>
2017-03-22 16:19:52 +0100

    initial import

---
+++ debugging/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+/**
+ * Created by moritz on 3/22/17.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        String data = "Line 1\nLine 2\nLine 3\nLine 4";
+
+        String[] strings = data.split("\n");
+        System.out.println(Arrays.toString(strings));
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println(strings[i]);
+        }
+    }
+}
